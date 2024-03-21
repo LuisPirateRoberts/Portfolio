96d9830f19e414faf453ba33d5deed77045c5191
Luis Pinto <lpinto1@ucol.com>
2024-03-21 00:50:38 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Hero } from "./components";
+
+export default function App() {
+  return (
+    <div className="bg-primary">
+      <Hero />
+    </div>
+  );
+}
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import About from "./About";
+import Works from "./Works";
+import Contact from "./Contact";
+
+export { Navbar, Footer, Hero, About, Works, Contact };
